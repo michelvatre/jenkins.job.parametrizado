0dad21220cef01aeb8a645e0ac526cee057a1783
michelvatre <140221258+michelvatre@users.noreply.github.com>
2023-08-01 20:47:59 -0600

    Update jobscript.sh

--- jobscript.sh
+++ jobscript.sh
@@ -4,7 +4,7 @@ if [ "$agente" = "true" ]
 then
 	echo "Prepárese para la misión agente X, que la fuerza te acompañe."
 else
-	echo "Disfruta el planeta humano/a $nombre, trata de no morir."
+	echo "Disfruta el planeta humano/a $nombre, trata de no morir. Ejemplo"
 fi
 echo "..."
 sleep 10
