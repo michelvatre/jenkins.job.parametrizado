afc2b7e697c133c4706840504c3c5d7785979aa6
michelvatre <140221258+michelvatre@users.noreply.github.com>
2023-08-01 20:58:16 -0600

    Update parametrizadoDSL2.groovy

--- parametrizadoDSL2.groovy
+++ parametrizadoDSL2.groovy
@@ -1,9 +1,9 @@
 job('ejemplo3-job-DSL') {
 	description('Job DSL de ejemplo para el curso de Jenkins')
   	scm {
-      		git('https://github.com/macloujulian/jenkins.job.parametrizado.git', 'main') { node ->
-        		node / gitConfigName('macloujulian')
-        		node / gitConfigEmail('macloujulian@gmail.com')
+      		git('https://github.com/michelvatre/jenkins.job.parametrizado.git', 'main') { node ->
+        		node / gitConfigName('michelvatre')
+        		node / gitConfigEmail('michelvatre94@gmail.com')
       		}
     	} 
   	parameters {
@@ -19,7 +19,7 @@ job('ejemplo3-job-DSL') {
     		shell("bash jobscript.sh")
     	}
   	publishers {
-      		mailer('macloujulian@gmail.com', true, true)
+      		mailer('michelvatre94@gmail.com', true, true)
       		slackNotifier {
 		  notifyAborted(true)
 		  notifyEveryFailure(true)
